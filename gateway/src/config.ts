07a4cd052f864742f6385f8d444cd0f20d2c3ecf
asmit990 <asmitpandeyw4354@gmail.com>
2026-06-01 00:59:52 +0530

    config for the prometheus

--- gateway/src/config.ts
+++ gateway/src/config.ts
@@ -1,22 +1,19 @@
 import dotenv from "dotenv"
+import fs from "fs"
+import yaml from "js-yaml"
 dotenv.config()
-
+const regionsFile = fs.readFileSync("../config/regions.yml", "utf8")
+const regionsConfig = yaml.load(regionsFile) as any
 
 const config = {
     port: process.env.PORT || 3000,
-
+    regions: regionsConfig.regions as Record<string, string>,
+    defaultRegion: regionsConfig.default_region as Record<string, string>,
     redis: {
        HOST: process.env.REDIS_HOST || "localhost",
        PORT: parseInt(process.env.REDIS_PORT || "6379"),
 
     }, 
-    regions: {
-        IN: process.env.IN_REGION || "http://localhost:8081",
-        US: process.env.US_REGION || "http://localhost:8082", 
-        GB: process.env.GB_REGION || "http://localhost:8083"
-    } as Record<string, string>,
-
-
     fallback: {
       IN: ["IN", "GB", "US"],
       US: ["US", "GB", "IN"],
